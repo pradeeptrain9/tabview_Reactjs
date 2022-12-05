9d699cf27853f65640f7ea5c29baf41abf29257c
pradeep <pradeeptrain9@gmail.com>
2022-12-05 16:39:38 +0530

    new changes

--- src/components/MultiTabView.js
+++ src/components/MultiTabView.js
@@ -2,30 +2,40 @@ import * as React from "react";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import Box from "@mui/material/Box";
+import { TabPanel, TabContext } from "@mui/lab";
 
 export default function MultiTabView() {
-  const [value, setValue] = React.useState("one");
+  const [value, setValue] = React.useState("1");
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
   return (
-    <Box sx={{ width: "100%" }}>
-      <Tabs
-        value={value}
-        onChange={handleChange}
-        textColor="secondary"
-        indicatorColor="secondary"
-        aria-label="secondary tabs example"
-        centered
-      >
-        <Tab value="one" label="PolicyDetails"></Tab>
-        <Tab value="two" label="PolicyConfigurations" />
-        <Tab value="three" label="Associations" />
-        <Tab value="four" label="Assigned Nodes"></Tab>
-        <Tab value="five" label="Assests"></Tab>
-      </Tabs>
+    <Box sx={{ width: "60%", typography: "body3" }}>
+      <TabContext value={value}>
+        <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            textColor="secondary"
+            indicatorColor="secondary"
+            aria-label="secondary tabs example"
+            centered
+          >
+            <Tab label="PolicyDetails" value="1" />
+            <Tab label="PolicyConfigurations" value="2" />
+            <Tab label="Associstions" value="3" />
+            <Tab label="AssignedNodes" value="4" />
+            <Tab label="Assets" value="5" />
+          </Tabs>
+        </Box>
+        <TabPanel value="1">Item One</TabPanel>
+        <TabPanel value="2">Item Two</TabPanel>
+        <TabPanel value="3">Item Three</TabPanel>
+        <TabPanel value="4">Item Four</TabPanel>
+        <TabPanel value="5">Item Five</TabPanel>
+      </TabContext>
     </Box>
   );
 }
